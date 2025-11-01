461872ca63658051bfdc75f1dc363e14362d2b44
Jungkyu Lim <59426408+DoxB@users.noreply.github.com>
2025-11-01 22:09:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.18 ms, Memory: 102 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String s : participant) {
+            if (!map.containsKey(s)) {
+                map.put(s, 1);
+            } else {
+                map.replace(s, map.get(s) + 1);
+            }
+        }
+        
+        for (String s : completion) {
+            map.replace(s, map.get(s) - 1);
+        }
+        
+        for (String s : map.keySet()) {
+            if (map.get(s) == 1) {
+                answer = s;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
