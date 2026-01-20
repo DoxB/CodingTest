97d9604df626146a05b25d62a3a393429da369d9
Jungkyu Lim <59426408+DoxB@users.noreply.github.com>
2026-01-20 17:04:09 +0900

    [Bronze I] Title: 이항 계수 1, Time: 88 ms, Memory: 14224 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+            dp[i][1] = i;
+            dp[i][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j < i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        System.out.println(dp[n][m]);
+    }
+}
